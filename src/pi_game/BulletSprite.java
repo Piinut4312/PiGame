ab279feebeb39654c0b4f2c4d27f64a2bbdf9531
LAPTOP-O5A0S4O9\ASUS <kevin4312cpes@gmail.com>
2021-05-15 18:05:23 +0800

    Add bullet system

---
+++ src/pi_game/BulletSprite.java
@@ -0,0 +1,23 @@
+package pi_game;
+
+import com.sun.javafx.geom.Vec2d;
+import javafx.scene.image.ImageView;
+
+public class BulletSprite extends GameSprite{
+
+    private double vel;
+
+    public BulletSprite(ImageView texture, double x, double y, double width, double height, double angle, double vel) {
+        super(texture, x, y, width, height, angle);
+        this.vel = vel;
+    }
+
+    public void update(){
+        this.move(new Vec2d(-vel*(Math.cos(Math.PI/2+this.getRadian())), -vel*(Math.sin(Math.PI/2+this.getRadian()))));
+        if(this.getDistanceTo(new Vec2d(PiGame.SCR_WIDTH/2, PiGame.SCR_HEIGHT/2)) < 10){
+            this.kill();
+        }
+        this.render();
+    }
+
+}
